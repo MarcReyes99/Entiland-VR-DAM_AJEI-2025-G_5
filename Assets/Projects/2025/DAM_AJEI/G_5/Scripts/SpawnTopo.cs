f686f8179b0cb0182a1fa3e5818fa11199e22317
Marctupadre <116338801+Marctupadre@users.noreply.github.com>
2025-03-12 19:43:35 +0100

    aaaaaaaaaaaaaaaa

--- Assets/Projects/2025/DAM_AJEI/G_5/Scripts/SpawnTopo.cs
+++ Assets/Projects/2025/DAM_AJEI/G_5/Scripts/SpawnTopo.cs
@@ -4,88 +4,22 @@ using UnityEngine;
 
 public class SpawnTopo : MonoBehaviour
 {
-    public List<GameObject> toposBuenos;
-    public List<GameObject> toposMalos;
-    public List<Transform> puntosDeSpawn;
+    public List<GameObject> puntosDeSpawn;
 
-    public float spawnTime = 0f;
-    public float spawnedTime = 4f;
-    public float tiempoDesaparicion = 3f;
+    public static SpawnTopo instance;
 
-    void Start()
+    private void Awake()
     {
-        DesactivarTopos();
-    }
-
-    void Update()
-    {
-        spawnTime += Time.deltaTime;
-
-        if (spawnTime >= spawnedTime)
+        if (instance == null)
         {
-            SpawnTopos();
-            spawnTime = 0f;
+            instance = this;
         }
-    }
-
-    void SpawnTopos()
-    {
-        DesactivarTopos();
-
-        int cantidadToposBuenos = Random.Range(1, 6);
-        int cantidadToposMalos = Random.Range(1, 6);
-
-        SpawnToposDeTipo(toposBuenos, cantidadToposBuenos);
-
-        SpawnToposDeTipo(toposMalos, cantidadToposMalos);
-    }
-
-    void SpawnToposDeTipo(List<GameObject> listaTopos, int cantidad)
-    {
-        for (int i = 0; i < cantidad; i++)
+        else if (instance != this)
         {
-            if (puntosDeSpawn.Count > 0 && listaTopos.Count > 0)
-            {
-                int randomPos = Random.Range(0, puntosDeSpawn.Count);
-                int randomTopo = Random.Range(0, listaTopos.Count);
-
-                Transform puntoSpawn = puntosDeSpawn[randomPos];
-                GameObject topo = listaTopos[randomTopo];
-
-                if (topo.activeSelf)
-                {
-                    topo.GetComponent<MeshRenderer>().enabled = false;
-                    topo.SetActive(false);
-                }
-
-                topo.SetActive(true);
-                topo.transform.position = puntoSpawn.position;
-                topo.GetComponent<MeshRenderer>().enabled = true;
-
-                StartCoroutine(DesactivarTopo(topo, tiempoDesaparicion));
-            }
-        }
-    }
-
-    IEnumerator DesactivarTopo(GameObject topo, float tiempo)
-    {
-        yield return new WaitForSeconds(tiempo);
-        topo.GetComponent<MeshRenderer>().enabled = false;
-        topo.SetActive(false);
-    }
-
-    void DesactivarTopos()
-    {
-        foreach (var topo in toposBuenos)
-        {
-            topo.SetActive(false);
-            topo.GetComponent<MeshRenderer>().enabled = false;
+            Destroy(this);
         }
 
-        foreach (var topo in toposMalos)
-        {
-            topo.SetActive(false);
-            topo.GetComponent<MeshRenderer>().enabled = false;
-        }
+        DontDestroyOnLoad(this);
     }
+
 }
--- Assets/Projects/2025/DAM_AJEI/G_5/Scripts/Topo.cs
+++ Assets/Projects/2025/DAM_AJEI/G_5/Scripts/Topo.cs
@@ -1,11 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class Topo : MonoBehaviour
 {
     public float spawnTime = 0f;
     public float spawnedTime = 4f;
+
+    public List<GameObject> spawnPoints;
+
+    public bool good;
+
+    int rand;
+    GameObject point; 
+
+    private void Start()
+    {
+
+        spawnPoints = SpawnTopo.instance.puntosDeSpawn;
+
+        rand = Random.Range(0, spawnPoints.Count);
+
+        point = spawnPoints[rand];
+    }
 
     void Update()
     {
@@ -22,11 +40,31 @@ public class Topo : MonoBehaviour
                 Appear();
             }
             spawnTime = 0f;
+            spawnedTime = Random.Range(3, 8);
         }
     }
 
     public void Appear()
     {
+        point.GetComponent<SpawnPoint>().inUse = false;
+
+        rand = Random.Range(0, spawnPoints.Count);
+
+        point = spawnPoints[rand];
+
+        while (point.GetComponent<SpawnPoint>().inUse == true)
+        {
+            rand = Random.Range(0, spawnPoints.Count);
+
+            point = spawnPoints[rand];
+        }
+
+        transform.position = point.transform.position;
+
+        transform.rotation = point.transform.rotation;
+
+        point.GetComponent<SpawnPoint>().inUse = true;
+
         gameObject.GetComponent<MeshRenderer>().enabled = true;
     }
 
